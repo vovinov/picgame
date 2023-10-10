da79abd16a5995ea389741c9f59771659303658d
vovinov <v.v.ovinov@yandex.ru>
2023-10-10 22:28:22 +0300

    v.0.0.6 - Add utils, some html/css upgrades

---
+++ data.py
@@ -0,0 +1,17 @@
+questions = [
+    {
+        'image': 'questions/images/shoushenk.jpg',
+        'answer1': 'Побег из Шоушенка',
+        'answer2': 'Властелин колец',
+        'answer3': 'Список Шиндлера',
+        'answer4': 'Криминальное чтиво',
+    },
+    {
+        'image': 'questions/images/shidler.jpg',
+        'answer1': 'Побег из Шоушенка',
+        'answer2': 'Властелин колец',
+        'answer3': 'Список Шиндлера',
+        'answer4': 'Криминальное чтиво',
+    },
+]
+
--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from data import questions
+
 
 def render_questions(request):
-    return render(request, 'questions/index.html')
+    return render(request, 'questions/index.html', context={"movies": questions})
